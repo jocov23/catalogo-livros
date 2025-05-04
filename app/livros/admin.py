59398350c0e9104d56fa0af0cc41b8db3d1b3f1b
jocov <jocov2309@gmail.com>
2025-05-04 18:09:39 -0300

    created superuser interface + minor changes

--- app/livros/admin.py
+++ app/livros/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Livro
 # Register your models here.
+
+admin.site.register(Livro)
+
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'ano_publicacao', 'editora')
+    search_fields = ( 'titulo', 'autor')
--- app/livros/models.py
+++ app/livros/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Livros(models.Model):
+class Livro(models.Model):
     titulo = models.CharField(max_length=200)
     autor = models.CharField(max_length=100)
     ano_publicacao = models.IntegerField()
