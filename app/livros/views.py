1e06539a2efd0f9d29752f111b3a387813715570
jocov <jocov2309@gmail.com>
2025-05-05 20:53:26 -0300

    added API implementation to allow CRUD operations

--- app/livros/views.py
+++ app/livros/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Livro
+from .serializers import LivroSerializer
 
-# Create your views here.
+class LivroViewSet(viewsets.ModelViewSet):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
