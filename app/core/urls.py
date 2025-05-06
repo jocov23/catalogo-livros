90a42dc40142e77fc203e0671509cf13d6fab1e4
jocov <jocov2309@gmail.com>
2025-05-06 13:51:06 -0300

    modified http status code + created a forms branch

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-
+#primary URLs
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('livros.urls')),
--- app/livros/views.py
+++ app/livros/views.py
@@ -5,31 +5,39 @@ from rest_framework import status
 from .models import Livro
 from .serializers import LivroSerializer
 
+
 #function to create or request data from a book
 @api_view(['GET', 'POST'])
 def livros_list(request):
+    
+    #show books by date of criation
+    livros = Livro.objects.all().order_by('-criado_em')
+
     #If method created is "get" (to return some data), serializer convert the list of books in JSON and return to the user
     if request.method == 'GET':
         livros = Livro.objects.all()
         serializer = LivroSerializer(livros, many = True)
         return Response(serializer.data)
+    
     #If method is "POST" (to create some data), serializer verify if the format is valid and or create succesfully(send to the database) 
     # or return error 400
     elif request.method == 'POST':
         serializer = LivroSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
     
-    # Function to modify or request data of a already existing book
+
+# Function to modify or request data of a already existing book
 @api_view(['GET', 'PUT', 'DELETE'])
 def livro_detail(request, pk):
+
     # Verify if the book requested is contained in the database (pk= primarykey)
     try:
         livro = Livro.objects.get(pk=pk)
     except Livro.DoesNotExist:
-            return Response({'erro': 'Livro nao encontrado'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'erro': 'Livro nao encontrado'}, status=404)
     
     #request data from a especific book by method GET
     if request.method == 'GET':
@@ -42,10 +50,10 @@ def livro_detail(request, pk):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=400)
 
     #if method is DELETE, it just...delete    
     elif request.method == 'DELETE':
         livro.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)       
+        return Response(status=204)       
     
