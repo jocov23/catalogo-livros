55f6f50601dab68817a71a97ebe457e80a64ff32
jocov <jocov2309@gmail.com>
2025-05-05 21:55:42 -0300

    enfeite

--- app/livros/views.py
+++ app/livros/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework import api_view
+from rest_framework import Response
+from rest_framework import status
 from .models import Livro
 from .serializers import LivroSerializer
 
